1a129647ad2667489c18564316f08f9973ce8f2d
Daria Sheibak <oksuroyy@gmail.com>
2022-11-02 09:52:36 +0300

    add files lab08 clean code

---
+++ LAB08_CLEANCODE/cleancode_practice/aircompany/Net/Aircompany/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Aircompany
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      Airport airport = new Airport(PlaneRegistry.Planes);
+      Airport militaryAirport = new Airport(airport.GetMilitaryPlanes());
+      Airport passengerAirport = new Airport(airport.GetPassengersPlanes());
+      using (StreamWriter writer = new StreamWriter("ConsoleOutputAircompanyLog.txt"))
+      {
+        writer.WriteLine(militaryAirport.SortPlanesByMaxFlightDistance().ToString());
+        writer.WriteLine(passengerAirport.SortPlanesByMaxSpeed().ToString());
+        writer.WriteLine(passengerAirport.GetPassengerPlaneWithMaxPassengersCapacity());
+      } 
+    }
+  }
+}
---
+++ LAB08_CLEANCODE/cleancode_practice/aircompany/Net/AircompanyTests/Tests/AirportTest.cs
@@ -0,0 +1,38 @@
+﻿using Aircompany;
+using Aircompany.Models;
+using Aircompany.Planes;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AircompanyTests.Tests
+{
+    [TestFixture]
+    public class AirportTest
+    {
+      private List<Plane> _planes = new List<Plane>(PlaneRegistry.Planes);
+      private PassengerPlane _planeWithMaxPassengerCapacity = new PassengerPlane("Boeing-747", 980, 16100, 70500, 242);
+      private List<Plane> _planesOrderedByMaxLoadCapacity = PlaneRegistry.Planes.OrderBy(x => x.GetMaxLoadCapacity()).ToList();
+     [Test]
+        public void TransportMilitaryPlanesCanBeDetected()
+        {
+            Airport airport = new Airport(_planes);
+            List<MilitaryPlane> transportMilitaryPlanes = airport.GetTransportMilitaryPlanes().ToList();
+            Assert.IsTrue(transportMilitaryPlanes.Count!=0);
+        }
+     [Test]
+        public void GetPassengerPlaneWithMaxPassengersCapacityCanBeDetected()
+        {
+            Airport airport = new Airport(_planes);
+            PassengerPlane expectedPlaneWithMaxPassengersCapacity = airport.GetPassengerPlaneWithMaxPassengersCapacity();
+            Assert.IsTrue(expectedPlaneWithMaxPassengersCapacity.Equals(_planeWithMaxPassengerCapacity));
+        }
+     [Test]
+        public void PlanesAreSortedByMaxLoadCapacity()
+        {
+            Airport airport = new Airport(_planes);
+            List<Plane> expectedPlanesSortedByMaxLoadCapacity = airport.SortPlanesByMaxLoadCapacity().GetPlanes().ToList();
+            Assert.IsTrue(expectedPlanesSortedByMaxLoadCapacity.SequenceEqual(_planesOrderedByMaxLoadCapacity));
+        }
+    }
+}
